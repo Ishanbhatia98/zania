e9e5d10377f3a8439cd8def04992905b1680ce89
ishan bhatia <ibhatia1998@gmail.com>
2025-02-01 16:47:55 +0530

    fix

--- app/test/main.py
+++ app/test/main.py
@@ -5,7 +5,6 @@ from app.database import db_instance
 
 def main():
     for file in [
-        # "pytest app/test/router/manage.py",
         "pytest app/test/model/product.py"
     ]:
         db_instance.delete_all_tables_and_metadata()
